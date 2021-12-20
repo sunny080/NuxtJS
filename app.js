4d779e74080b329b80773eef171850d3d66a7790
Sunny <77973332+sunny080@users.noreply.github.com>
2021-12-20 06:30:32 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.querySelector("#bhem").style.marginLeft = "-0px";
+}
+  
+function closeNav() {
+    document.querySelector("#bhem").style.marginLeft = "-200px";
+}
